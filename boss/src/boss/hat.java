ce824da725fb7d5d9047ab5178f5f93d844e488d
bikram793 <bikramjoshi7151@gmail.com>
2022-04-18 21:41:14 +0530

    added gui

--- boss/src/boss/hat.java
+++ boss/src/boss/hat.java
@@ -1,11 +1,24 @@
 package boss;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
 public class hat {
-
-	public hat() {
-		int a=0;
-		int b=4;
-		// TODO Auto-generated constructor stub
-	}
-
+public static void main(String[] args) {
+	/*
+	JFrame frame= new JFrame();//create frame
+	frame.setTitle("Boss");//sets title
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application we can keep (JFrame.DO_NOTHING_ON_CLOSE)
+	
+	frame.setResizable(false); //prevent screen from resize
+	frame.setSize(420,420);//sets x-dimesnsion,y-dimension of frame
+	frame.setVisible(true);//make frame visible
+ImageIcon image = new ImageIcon("boss.jpg");//create an image icon
+frame.setIconImage(image.getImage());//change icon of frame
+frame.getContentPane().setBackground(new Color(74,233,36));//change color of background for default color(Color.green)
+		*/
+	MyFrame myFrame=new MyFrame();
+}
 }
