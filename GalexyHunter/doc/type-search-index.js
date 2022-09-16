25e483ef35b8d6de9f9834b50df6249915cf09c7
bikram793 <bikramjoshi7151@gmail.com>
2022-09-16 21:15:05 +0530

    2nd commit

---
+++ GalexyHunter/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Alien"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Board"},{"p":"<Unnamed>","l":"CollisionEx"},{"p":"<Unnamed>","l":"Missile"},{"p":"<Unnamed>","l":"SpaceShip"},{"p":"<Unnamed>","l":"Sprite"}];updateSearchResults();
