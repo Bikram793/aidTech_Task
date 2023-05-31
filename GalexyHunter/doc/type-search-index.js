48b0c8b29bf7f91d0c0ef4ac25a151e0784cf4c2
bikram7151 <66201082+Bikram793@users.noreply.github.com>
2023-05-31 14:14:40 +0530

    Delete GalexyHunter directory

--- GalexyHunter/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Alien"},{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Board"},{"p":"<Unnamed>","l":"CollisionEx"},{"p":"<Unnamed>","l":"Missile"},{"p":"<Unnamed>","l":"SpaceShip"},{"p":"<Unnamed>","l":"Sprite"}];updateSearchResults();
