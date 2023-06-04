6c86b1793dac458412551cfa61093f0253dc1572
bikram793 <bikramjoshi7151@gmail.com>
2023-06-04 17:27:29 +0530

    Frist commit

---
+++ Demo/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+public static void main(String[] args) {
+	
+	int a=10;
+	float b=10.0f;
+	String str="Bikram";
+	char c='b';
+	boolean bol=true;
+	
+	System.out.println(a+" "+b+" "+ str+" "+c+" "+bol);
+	
+}
+}
